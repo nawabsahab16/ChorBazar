d133514816a102e2a1c371205370631fc66c093a
nawabsahab16 <Nawab162004@gmail.com>
2023-12-21 00:08:50 +0530

    feat: react-scroll and recat-icons set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        PrimaryColor : "#f5ebe0",
+        SecondaryColor : "#e3d5ca",
+        DarkColor: "#d4a373",
+        ExtraDarkColor : "#a98467",
+      },
+    },
   },
   plugins: [],
 }
